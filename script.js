2ce5d7e37c6264cc8e0617e8a74748ec770964e1
viobi <vivsk0806@gmail.com>
2025-07-21 20:36:13 -0400

    comentarios finales

--- script.js
+++ script.js
@@ -27,17 +27,20 @@ class Estudiante {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    //si estudiantes ya fue inicializado
     if (!localStorage.getItem('estudiantes')) {
         cargarestudiantes();
     }
 
+    //si se desea iniciar sesion 
     const formlogin = document.getElementById('login-form');
-
+    
     if (formlogin) {
         formlogin.addEventListener('submit', async function (event) {
             event.preventDefault();
             const username = document.getElementById('nombreusuario').value;
             const password = document.getElementById('contrasena').value;
+            //toma el usuario y contasena proporcionados, los busca en estudiantes
             const user = buscarusuario(username, password);
             if (user) {
                 localStorage.setItem('loggedUser', JSON.stringify(user));
@@ -52,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    //si desea registrar un nuevo usuario
     const formregistro = document.getElementById('register-form');
 
     if (formregistro) {
@@ -65,6 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const confirmarcontrasena = document.getElementById('confirmar').value;
             let estudiantes = JSON.parse(localStorage.getItem('estudiantes')) || [];
 
+            //validaciones del formulario
             if (password !== confirmarcontrasena) {
                 alert("Error: Las contraseñas no coinciden.")
                 return;
@@ -81,6 +86,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 return;
             }
             else{
+                //todo bien, crea estudiante y lo agrega a estudiantes
                 const newstudent = new Estudiante(name, username, email, password, []);
                 estudiantes.push(newstudent);
                 localStorage.setItem('estudiantes', JSON.stringify(estudiantes));
@@ -93,6 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const user = JSON.parse(localStorage.getItem('loggedUser'));
 
+    //si toca el icono de perfil
     const perfilbtn = document.getElementById('user-icon');
     if (perfilbtn) {
         perfilbtn.addEventListener('click', function (e) {
@@ -105,6 +112,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    //saca el icono de logout del catalogo si no hay sesion iniciada
     if (window.location.pathname.endsWith('CatalogoCursos.html')) {
         if (user) {
             document.getElementById('logout-icon').style.display = 'inline';
@@ -112,6 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
         mostrarCursos();
     }
 
+    //si se presiona el icono logout
     const logoutBtn = document.getElementById('logout-icon');
     if (logoutBtn) {
         logoutBtn.addEventListener('click', function () {
@@ -125,11 +134,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+//funcion para validad que el texto contenga letras y no numeros, sacada de internet
 function validarsinumero(texto) {
     const regex = /^[a-zA-Z\s]+$/;
     return regex.test(texto);
 }
 
+//funcion para leer los datos del json de estudiantes
 async function cargarestudiantes() {
     try {
         const response = await fetch('estudiantes.json');
@@ -142,17 +153,21 @@ async function cargarestudiantes() {
     }
 }
 
+//fucncion para buscar un usuario en el arreglo estudiantes
 function buscarusuario(username, password) {
     const estudiantes = JSON.parse(localStorage.getItem('estudiantes')) || [];
     return estudiantes.find(est => est.usuario === username && est.contrasena === password);
 }
 
+//funcion para que los cursos se muestren en el catalogo 
+//ademas lectura del json cursos
 async function mostrarCursos() {
     try {
         const response = await fetch('cursos.json');
         const cursos = await response.json();
         const cursosContainer = document.getElementById('cursos-container');
 
+        //crea tarjetitas de cursos
         cursos.forEach(curso => {
             const cursoElement = document.createElement('div');
             cursoElement.classList.add('curso');
@@ -171,6 +186,7 @@ async function mostrarCursos() {
     }
 }
 
+//si se presiona el boton de inscribirse en algun curso del catalogo
 function inscribirCurso(id) {
     const user = JSON.parse(localStorage.getItem('loggedUser'));
     if (!user) {
@@ -178,6 +194,7 @@ function inscribirCurso(id) {
         return;
     }
 
+    //valida que el usuario ya este inscrito en el curso seleccionado
     const misCursosObj = JSON.parse(localStorage.getItem('misCursos')) || {};
     misCursos = misCursosObj[user.usuario] || [];
     if (misCursos.includes(id)) {
@@ -187,7 +204,7 @@ function inscribirCurso(id) {
     window.location.href = `ConfirmacionCurso.html?curso=${id}`;
 }
 
-
+//informacion del curso para inscribir
 if (window.location.pathname.endsWith('ConfirmacionCurso.html')) {
     document.addEventListener('DOMContentLoaded', async () => {
         const params = new URLSearchParams(window.location.search);
@@ -195,6 +212,7 @@ if (window.location.pathname.endsWith('ConfirmacionCurso.html')) {
         const resp = await fetch('cursos.json');
         const cursos = await resp.json();
         const curso = cursos.find(c => c.id === cursoId);
+        //pase de todos los datos del curso
         document.getElementById('curso-nombre').innerText = curso.nombre;
         document.getElementById('curso-profesor').innerText = 'Profesor: ' + curso.profesor;
         document.getElementById('curso-descripcion').innerText = curso.descripcion;
@@ -215,14 +233,17 @@ if (window.location.pathname.endsWith('ConfirmacionCurso.html')) {
             li.innerText = item;
             listatemario.appendChild(li);
         });
+        //botones de confirmacion y cancelar solitiud de inscripcion
         document.getElementById('confirm-btn').addEventListener('click', () => {
             const user = JSON.parse(localStorage.getItem('loggedUser'));
             if (!user) return alert('Debe iniciar sesión');
             let misCursos = JSON.parse(localStorage.getItem('misCursos')) || {};
             misCursos[user.usuario] = misCursos[user.usuario] || []; //paque no hayan 2 cursitos iguales
             if (!misCursos[user.usuario].includes(cursoId)) {
+                //agg curso nuevo a miscursos del usuario
                 misCursos[user.usuario].push(cursoId);
                 localStorage.setItem('misCursos', JSON.stringify(misCursos));
+                //actualiza estudiantes con el nuevo curso del usuario
                 const estudiantes = JSON.parse(localStorage.getItem('estudiantes')) || [];
                 const index = estudiantes.findIndex(est => est.usuario === user.usuario);
                 console.log("ayudame: " + user.usuario);
@@ -238,8 +259,10 @@ if (window.location.pathname.endsWith('ConfirmacionCurso.html')) {
     });
 }
 
+//si se presiona el icono de perfil 
 if (window.location.pathname.endsWith('perfil.html')) {
     document.addEventListener('DOMContentLoaded', () => {
+        //pase de datos de la informacion del perfil
         const user = JSON.parse(localStorage.getItem('loggedUser'));
         const cont = document.querySelector('.infoperfil');
         const div = document.createElement('div');
@@ -252,6 +275,7 @@ if (window.location.pathname.endsWith('perfil.html')) {
     });
 }
 
+//si se presiona seccion mis cursos
 if (window.location.pathname.endsWith('MisCursos.html')) {
     document.addEventListener('DOMContentLoaded', async () => {
         const user = JSON.parse(localStorage.getItem('loggedUser'));
@@ -261,6 +285,7 @@ if (window.location.pathname.endsWith('MisCursos.html')) {
         else {
             document.getElementById('logout-icon').style.display = 'inline';
         }
+        //pase de datos de los cursos del usuario logeado a misCursos
         const misCursos = JSON.parse(localStorage.getItem('misCursos')) || {};
         const ids = misCursos[user.usuario] || [];
         if (ids.length === 0) {
@@ -273,6 +298,7 @@ if (window.location.pathname.endsWith('MisCursos.html')) {
         ids.forEach(id => {
             const curso = cursos.find(c => c.id === id);
             if (curso) {
+                //crea tarjetita de curso 
                 const div = document.createElement('div');
                 div.classList.add('curso');
                 div.innerHTML = `
@@ -288,6 +314,7 @@ if (window.location.pathname.endsWith('MisCursos.html')) {
     });
 }
 
+//si se presiona ver detalles en un curso
 if (window.location.pathname.endsWith('CursoDetalle.html')) {
     document.addEventListener('DOMContentLoaded', async () => {
         const params = new URLSearchParams(window.location.search);
@@ -296,6 +323,7 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
         const cursos = await resp.json();
         const user = JSON.parse(localStorage.getItem('loggedUser'));
         const curso = cursos.find(c => c.id === cursoId);
+        //pase de datos a la interfaz
         document.getElementById('curso-nombre').innerText = curso.nombre;
         document.getElementById('curso-profesor').innerText = 'Profesor: ' + curso.profesor;
         document.getElementById('curso-descripcion').innerText = curso.descripcion;
@@ -316,6 +344,7 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
             li.innerText = item;
             listatemario.appendChild(li);
         });
+        //incrustar enlaces, pdfs y videos del curso (materiales)
         const listamateriales = document.getElementById('materiales-lista');
         listamateriales.innerHTML = '';
         curso.materiales.forEach(item => {
@@ -332,9 +361,12 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
             }
             listamateriales.appendChild(divmateriales);
         });
+
+        //pase del proyecto final
         document.getElementById('proyectotitulo').innerText = curso.proyecto_final.titulo;
         document.getElementById('proyecto-enunciado').innerText = curso.proyecto_final.enunciado;
 
+        //detalles de calificaciones, crea seccion calificaciones una vez se hizo una entrega
         const entregas = JSON.parse(localStorage.getItem('entregas')) || {};
         const entregaActual = user && entregas[user.usuario] && entregas[user.usuario][cursoId];
         const statusDiv = document.getElementById('project-status');
@@ -350,14 +382,14 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
             const calificacionDiv = document.createElement('div');
             calificacionDiv.innerHTML = `
                 <br>
-                <h2>Calificación</h2>
+                <h2 id="calificaciones">Calificación</h2>
                 <p><strong>Nota: </strong>${entregaActual.calificacion || 'No disponible'}</p>
                 <p><strong>Comentarios: </strong>${entregaActual.comentarios || 'No hay comentarios'}</p>
             `;
             cont.appendChild(calificacionDiv);
         }
 
-
+        //genera datos de la entrega una vez se sube el archivo
         const form = document.getElementById('final-project-form');
         form.addEventListener('submit', event => {
             event.preventDefault();
@@ -370,7 +402,9 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
             //supuesto guardado de entrega en localStorage
             let entregas = JSON.parse(localStorage.getItem('entregas')) || {};
 
+            //genera calificacion random
             const calificacion = Math.floor(Math.random() * 21);
+            //crea un comentario segun calificacion
             let comentario = '';
             if (calificacion >= 16) {
                 comentario = '¡Muy buen trabajo!';
@@ -380,7 +414,7 @@ if (window.location.pathname.endsWith('CursoDetalle.html')) {
                 comentario = 'Lo peor que he visto en mi vida.';
             }
 
-
+            //crea entrega
             entregas[user.usuario] = entregas[user.usuario] || {};
             entregas[user.usuario][cursoId] = {
                 fileName: file.name,
